60e7a7a2f3bb362e351bec24fe5ae7b1319061c2
Mayank <mayankindoliya@gmail.com>
2024-04-03 11:55:32 +0530

    changes

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <main>
+    <div id='game-container'>
+      PLAYERS
+
+      GAME BOARD
+    </div>
+
+    LOG
+  </main>
+}
+
+export default App
